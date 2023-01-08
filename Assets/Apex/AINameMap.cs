1b8a5e281ae4e040573e66ac532457880a6ab506
Hlib <zadachings@gmail.com>
2023-01-08 18:37:52 +0200

    added bull ai

--- Assets/Apex/AINameMap.cs
+++ Assets/Apex/AINameMap.cs
@@ -12,6 +12,7 @@ namespace Apex.AI
 		public static readonly Guid BaseAttck = new Guid("f8302355-e2b6-4b15-8f31-cdb656017bf3");
 		public static readonly Guid BaseEnemy = new Guid("b733226d-9e53-4c8f-be29-f8abc9712f69");
 		public static readonly Guid BaseEnemyScan = new Guid("fec13c95-8eac-47d6-9475-bca0b4de0018");
+		public static readonly Guid BullAI = new Guid("ba617324-be08-4f7f-924d-7597bbf3ad42");
 		public static readonly Guid SpyEnemy = new Guid("e6fc5f07-8ee4-4a75-9962-ce8dc82f8f7c");
 
     }
