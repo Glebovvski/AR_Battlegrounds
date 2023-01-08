93489dd442f12851717045ec460d66f2a1119e86
Hlib <zadachings@gmail.com>
2023-01-08 18:17:03 +0200

    added Bull enemy script

---
+++ Assets/Razeware/Scripts/Enemy/BullEnemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class BullEnemy : Enemy
+    {
+        public override void StartAttack()
+        {
+            animationController.Attack();
+        }
+
+        public void Attack()
+        {
+            if (AttackTarget == null)
+                return;
+
+            AttackTarget.Defense.TakeDamage(AttackForce);
+        }
+    }
+}
