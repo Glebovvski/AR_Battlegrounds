65b0f78ffbb01b4a3b799210e440bf2b37841349
Hlib <zadachings@gmail.com>
2023-01-09 17:29:03 +0200

    Update MissileLauncher.cs

--- Assets/Razeware/Scripts/Defence/MissileLauncher.cs
+++ Assets/Razeware/Scripts/Defence/MissileLauncher.cs
@@ -10,6 +10,7 @@ namespace Defendable
     {
         protected override bool IsReady { get => Time.time - lastShotTime > RelaodTime; set => IsReady = value; }
 
+        private List<EnemyTimePositionInfo> enemyPositions = new List<EnemyTimePositionInfo>();
         protected override void Attack(Enemy enemy)
         {
             var bullet = PoolManager.Instance.GetFromPool<LaunchableMissile>(PoolObjectType.LaunchableMissile);
@@ -26,8 +27,23 @@ namespace Defendable
             var targetRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z), tower.up);
             tower.rotation = Quaternion.RotateTowards(tower.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
+            enemyPositions.Add(new EnemyTimePositionInfo(Time.time, enemy.Position));
+
             if (Quaternion.Angle(targetRotation, tower.rotation) <= angleThreshold) return true;
             return false;
         }
+    }
+}
+
+
+public class EnemyTimePositionInfo
+{
+    public float Timestamp{get;private set;}
+    public Vector3 Position {get;private set;}
+
+    public EnemyTimePositionInfo(float time, Vector3 position)
+    {
+        Timestamp = time;
+        Position = position;
     }
 }
