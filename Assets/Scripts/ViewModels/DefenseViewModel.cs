120730a95a25d5d6d26a4c6343234f0472321dc6
Hlib <zadachings@gmail.com>
2023-01-10 21:11:51 +0200

    Update DefenseViewModel.cs

--- Assets/Scripts/ViewModels/DefenseViewModel.cs
+++ Assets/Scripts/ViewModels/DefenseViewModel.cs
@@ -1,13 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Defendable;
 using UnityEngine;
+using Zenject;
 
 public class DefenseViewModel
 {
-    
-    public DefenseViewModel(Parameters)
+    private CurrencyModel CurrencyModel { get; set; }
+
+    private DefenseView DefenseView { get; set; }
+    private ScriptableDefence Defence { get; set; }
+
+    [Inject]
+    private void Construct(CurrencyModel currencyModel)
     {
-        
+        CurrencyModel = currencyModel;
+    }
+
+    public DefenseViewModel(DefenseView view)
+    {
+        DefenseView = view;
+        CurrencyModel.OnGoldAmountChanged += UpdateDefenseAffordable;
+    }
+
+    private void UpdateDefenseAffordable()
+    {
+
     }
 }
