127521bedafb23d99a2a1a086feed5b0818ec270
Hlib <zadachings@gmail.com>
2023-01-10 10:51:46 +0200

    wip  kamikaze enemy

--- Assets/Razeware/Scripts/Enemy/KamikazeEnemy.cs
+++ Assets/Razeware/Scripts/Enemy/KamikazeEnemy.cs
@@ -1,14 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Defendable;
 using UnityEngine;
 
 namespace Enemies
 {
     public class KamikazeEnemy : Enemy
     {
+        private LayerMask defenseMask;
         public override void StartAttack()
         {
-            throw new System.NotImplementedException();
+            Explode();
+        }
+
+        private void Explode()
+        {
+            defenseMask = LayerMask.GetMask("Defense");
+            var colliders = Physics.OverlapSphere(this.transform.position, ScanRange, defenseMask);
+            foreach (var collider in colliders)
+            {
+                if(collider.TryGetComponent<Defense>(out var defense))
+                {
+                    defense.TakeDamage(AttackForce);
+                }
+            }
         }
     }
 }
