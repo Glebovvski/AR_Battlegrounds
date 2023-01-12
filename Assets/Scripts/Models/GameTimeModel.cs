51e17db8e25ea417a9bfa6855bd9841d00145245
Hlib <zadachings@gmail.com>
2023-01-12 18:40:20 +0200

    Update GameTimeModel.cs

--- Assets/Scripts/Models/GameTimeModel.cs
+++ Assets/Scripts/Models/GameTimeModel.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Defendable;
 using UnityEngine;
+using Zenject;
 
 public class GameTimeModel : MonoBehaviour
 {
+    private DefensesModel DefensesModel { get; set; }
+
     public bool IsPaused = Time.timeScale == 0;
+
+    [Inject]
+    private void Construct(DefensesModel defensesModel)
+    {
+        DefensesModel = defensesModel;
+    }
+
     private void Pause()
     {
         Time.timeScale = 0;
