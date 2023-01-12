07ab46e2c5e7a755e6f95c05f9ca7ab2d6fd0c21
Hlib <zadachings@gmail.com>
2023-01-12 18:07:09 +0200

    Update DefenseViewFactory.cs

--- Assets/Scripts/DefenseViewFactory.cs
+++ Assets/Scripts/DefenseViewFactory.cs
@@ -5,19 +5,21 @@ using Zenject;
 public class DefenseViewFactory : PlaceholderFactory<DefenseView>
 {
     private CurrencyModel CurrencyModel { get; set; }
+    private DiContainer DiContainer { get; set; }
 
     [Inject]
-    private void Construct(CurrencyModel currencyModel)
+    private void Construct(CurrencyModel currencyModel, DiContainer container)
     {
+        DiContainer = container;
         CurrencyModel = currencyModel;
     }
 
     public DefenseView CreateDefenseView(ScriptableDefense defense, DefenseView prefab, Transform parent)
     {
-        if(defense.Price == 0) return null;
-        
+        if (defense.Price == 0) return null;
+
         var view = GameObject.Instantiate(prefab, parent);
-        DefenseViewModel vm = new DefenseViewModel(defense, view, CurrencyModel);
+        DefenseViewModel vm = DiContainer.Resolve<DefenseViewModel>();// new DefenseViewModel(defense, view, CurrencyModel);
         view.Init(defense, vm);
         return view;
     }
