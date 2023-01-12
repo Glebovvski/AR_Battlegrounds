63ff9667a8ffc7bb23e0b7aaaa35f27c6415598a
Hlib <zadachings@gmail.com>
2023-01-12 18:45:07 +0200

    Update GameTimeModelInstaller.cs

--- Assets/Scripts/Infrastructure/GameTimeModelInstaller.cs
+++ Assets/Scripts/Infrastructure/GameTimeModelInstaller.cs
@@ -5,5 +5,6 @@ public class GameTimeModelInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
+        Container.Bind<IInitializable>().To<GameTimeModel>().AsSingle();
     }
 }
