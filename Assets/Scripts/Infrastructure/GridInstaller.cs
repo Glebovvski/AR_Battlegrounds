e54e22086425deed121514e56e982c1c627a4db6
Hlib <zadachings@gmail.com>
2023-01-13 08:20:53 +0200

    added GridInstaller

---
+++ Assets/Scripts/Infrastructure/GridInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+
+public class GridInstaller : MonoInstaller
+{
+    [SerializeField] private GameGrid grid;
+    
+    public override void InstallBindings()
+    {
+        Container.Bind<GameGrid>().FromInstance(grid).AsSingle();
+    }
+}
