5aedea64c41101c6e6457cf85ffabe59c750cbcb
Hlib <zadachings@gmail.com>
2023-01-15 14:03:07 +0200

    Update HealerEnemy.cs

--- Assets/Scripts/Enemy/HealerEnemy.cs
+++ Assets/Scripts/Enemy/HealerEnemy.cs
@@ -7,11 +7,13 @@ namespace Enemies
     public class HealerEnemy : Enemy
     {
         [SerializeField] private ParticleSystem healFx;
+        private bool isHealFxPlaying = false;
         public void Heal()
         {
             Debug.LogError("HEALER FOLLOW: " + this.FollowTarget.name);
             var colliders = Physics.OverlapSphere(Position, AttackRadius, LayerMask.GetMask("Enemy"));
-            HealFX();
+            if (!isHealFxPlaying)
+                HealFX();
             foreach (var collider in colliders)
             {
                 if (collider.TryGetComponent<Enemy>(out var enemy))
@@ -36,13 +38,15 @@ namespace Enemies
 
         IEnumerator PlayHeal()
         {
+            isHealFxPlaying = true;
             healFx.Play();
-            for(float t = 0; t < 1;)
+            for (float t = 0; t < 1;)
             {
-                t+=Time.deltaTime;
+                t += Time.deltaTime;
                 yield return null;
             }
             healFx.Stop();
+            isHealFxPlaying = false;
         }
     }
 }
