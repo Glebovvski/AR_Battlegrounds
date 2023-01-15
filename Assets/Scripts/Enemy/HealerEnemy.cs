3f927fc1346796f05e747669a4581e1406184e97
Hlib <zadachings@gmail.com>
2023-01-15 13:45:29 +0200

    fixed healer fx

--- Assets/Scripts/Enemy/HealerEnemy.cs
+++ Assets/Scripts/Enemy/HealerEnemy.cs
@@ -11,7 +11,7 @@ namespace Enemies
         {
             Debug.LogError("HEALER FOLLOW: " + this.FollowTarget.name);
             var colliders = Physics.OverlapSphere(Position, AttackRadius, LayerMask.GetMask("Enemy"));
-            healFx.Play();
+            HealFX();
             foreach (var collider in colliders)
             {
                 if (collider.TryGetComponent<Enemy>(out var enemy))
@@ -27,6 +27,22 @@ namespace Enemies
             base.StartAttack(out isReady);
             if (isReady)
                 Heal();
+        }
+
+        public void HealFX()
+        {
+            StartCoroutine(PlayHeal());
+        }
+
+        IEnumerator PlayHeal()
+        {
+            healFx.Play();
+            for(float t = 0; t < 1;)
+            {
+                t+=Time.deltaTime;
+                yield return null;
+            }
+            healFx.Stop();
         }
     }
 }
