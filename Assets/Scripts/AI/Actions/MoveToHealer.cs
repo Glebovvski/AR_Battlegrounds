d96d743841ae6814825277c5647c6f9831b5ca8a
Hlib <zadachings@gmail.com>
2023-01-16 17:34:37 +0200

    added Move to Healer action

---
+++ Assets/Scripts/AI/Actions/MoveToHealer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using AI;
+using Apex.AI;
+using Enemies;
+using UnityEngine;
+
+public class MoveToHealer : ActionBase
+{
+    public override void Execute(IAIContext context)
+    {
+        var c = (AIContext)context;
+        var enemy = c.Enemy;
+
+        var healer = Enemies.AIManager.Instance.GetClosestEnemyByType(enemy, EnemyType.Healer);
+        if (healer && healer.isActiveAndEnabled)
+        {
+            enemy.FollowTarget = healer;
+            enemy.MoveTo(enemy.FollowTarget.Position);
+        }
+    }
+}
