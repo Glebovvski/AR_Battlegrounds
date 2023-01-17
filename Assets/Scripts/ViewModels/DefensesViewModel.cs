c7f1880dc2e0e7140189d1f52ca25ef9bef5adac
Hlib <zadachings@gmail.com>
2023-01-17 18:02:06 +0200

    Update DefensesViewModel.cs

--- Assets/Scripts/ViewModels/DefensesViewModel.cs
+++ Assets/Scripts/ViewModels/DefensesViewModel.cs
@@ -8,20 +8,24 @@ public class DefensesViewModel : MonoBehaviour
     [SerializeField] private DefenseView prefab;
     [SerializeField] private Transform viewParent;
     private DefensesModel DefensesModel { get; set; }
+    private LoseModel LoseModel { get; set; }
     private DiContainer Container { get; set; }
 
     public event Action OnDefenseSelected;
 
     [Inject]
-    private void Construct(DefensesModel defensesModel, DiContainer container)
+    private void Construct(DefensesModel defensesModel, LoseModel loseModel, DiContainer container)
     {
         Container = container;
         DefensesModel = defensesModel;
+        LoseModel = loseModel;
     }
 
     private void Start()
     {
         DefensesModel.OnSelectDefenseClick += DefenseSelected;
+        LoseModel.OnRestart += DefenseSelected;
+
         DefenseViewFactory factory = Container.Resolve<DefenseViewFactory>();
         foreach (var defense in DefensesModel.List)
         {
