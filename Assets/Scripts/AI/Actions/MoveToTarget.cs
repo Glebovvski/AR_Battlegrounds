f3b6b933db3ff053e74f42ecd1673bdb8b52fff3
Hlib <zadachings@gmail.com>
2023-01-19 08:34:37 +0200

    Update MoveToTarget.cs

--- Assets/Scripts/AI/Actions/MoveToTarget.cs
+++ Assets/Scripts/AI/Actions/MoveToTarget.cs
@@ -9,6 +9,7 @@ public class MoveToTarget : ActionBase
     public override void Execute(IAIContext context)
     {
         var c = (AIContext)context;
-        c.Enemy.MoveTo(c.Enemy.AttackTarget.Position);
+        if (c.Enemy.AttackTarget != null && c.Enemy.AttackTarget.IsAlive)
+            c.Enemy.MoveTo(c.Enemy.AttackTarget.Position);
     }
 }
