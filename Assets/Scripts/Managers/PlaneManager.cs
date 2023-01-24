a300071b46e2f4c360332dae71d4d9968e67a96c
Hlib <zadachings@gmail.com>
2023-01-24 19:48:14 +0200

    Update PlaneManager.cs

--- Assets/Scripts/Managers/PlaneManager.cs
+++ Assets/Scripts/Managers/PlaneManager.cs
@@ -10,17 +10,20 @@ public class PlaneManager : MonoBehaviour
     [SerializeField] private ARPlaneManager planeManager;
     [SerializeField] private ARSessionOrigin origin;
 
-    private void Start()
+    private void Update()
     {
-        planeManager.planesChanged += PlanesChanged;
-    }
-
-    private void PlanesChanged(ARPlanesChangedEventArgs obj)
-    {
-        if(obj.added.Count == 0) return;
-
-        var plane = obj.added[0];
-        origin.MakeContentAppearAt(this.transform, plane.center, Quaternion.identity);
-        planeManager.requestedDetectionMode = PlaneDetectionMode.None;
+        var touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(touch.position);
+            if (Physics.Raycast(raycast, out RaycastHit raycastHit))
+            {
+                if (raycastHit.collider.TryGetComponent<ARPlane>(out var plane))
+                {
+                    origin.MakeContentAppearAt(this.transform, plane.center, Quaternion.identity);
+                    planeManager.requestedDetectionMode = PlaneDetectionMode.None;
+                }
+            }
+        }
     }
 }
