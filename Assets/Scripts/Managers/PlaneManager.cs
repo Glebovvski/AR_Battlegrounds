b1561c67d494cdc1e5188c3dd0473e951f5f90cf
Hlib <zadachings@gmail.com>
2023-01-25 13:16:19 +0200

    wip

--- Assets/Scripts/Managers/PlaneManager.cs
+++ Assets/Scripts/Managers/PlaneManager.cs
@@ -10,8 +10,11 @@ public class PlaneManager : MonoBehaviour
     [SerializeField] private ARPlaneManager planeManager;
     [SerializeField] private ARSessionOrigin origin;
     [SerializeField] private GameGrid Grid;
+    [SerializeField] private ARRaycastManager arRaycastManager;
 
     private bool gridCreated = false;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     private void Start()
     {
@@ -27,22 +30,29 @@ public class PlaneManager : MonoBehaviour
         var touch = Input.GetTouch(0);
         if (touch.phase == TouchPhase.Began)
         {
-            Ray raycast = Camera.main.ScreenPointToRay(touch.position);
-            if (Physics.Raycast(raycast, out RaycastHit raycastHit, float.MaxValue))
+            if (arRaycastManager.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
             {
-                if (raycastHit.collider.TryGetComponent<ARPlane>(out var plane))
-                {
-                    // this.transform.localScale = new Vector3(this.transform.localScale.x / plane.size.x, 1, this.transform.localScale.z / plane.size.y);
-                    this.transform.SetParent(plane.transform);
-                    origin.MakeContentAppearAt(this.transform, plane.center, Quaternion.identity);
-                    Grid.CreateGrid();
-                    planeManager.requestedDetectionMode = PlaneDetectionMode.None;
-                    gridCreated = true;
-                    DebugView.Instance.SetText("GRID CREATED");
-                }
-                else
-                    DebugView.Instance.SetText("NO AR PLANE");
+                var hitPose = hits[0].pose;
+                this.transform.position = hitPose.position;
+                this.transform.rotation = hitPose.rotation;
+                Grid.CreateGrid();
+                DebugView.Instance.SetText("GRID CREATED");
+                gridCreated = true;
+                planeManager.requestedDetectionMode = PlaneDetectionMode.None;
             }
+            // Ray raycast = Camera.main.ScreenPointToRay(touch.position);
+            // if (Physics.Raycast(raycast, out RaycastHit raycastHit, float.MaxValue))
+            // {
+            //     if (raycastHit.collider.TryGetComponent<ARPlane>(out var plane))
+            //     {
+            //         // this.transform.localScale = new Vector3(this.transform.localScale.x / plane.size.x, 1, this.transform.localScale.z / plane.size.y);
+            //         // this.transform.SetParent(plane.transform);
+            //         origin.MakeContentAppearAt(this.transform, plane.center, Quaternion.identity);
+            //         DebugView.Instance.SetText("GRID CREATED");
+            //     }
+            //     else
+            //         DebugView.Instance.SetText("NO AR PLANE");
+            // }
             else
                 DebugView.Instance.SetText("NO RAYCAST TARGET");
         }
