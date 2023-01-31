11b9d7325f8c76ae411e218bbec0eb29d393d698
Hlib <zadachings@gmail.com>
2023-01-31 11:55:54 +0200

    wip

--- Assets/Scripts/Views/MenuView.cs
+++ Assets/Scripts/Views/MenuView.cs
@@ -2,22 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 public class MenuView : MonoBehaviour
 {
+    private MenuViewModel MenuViewModel { get; set; }
+
     [SerializeField] private GameObject menuPanel;
 
     [SerializeField] private Button startBtn;
     [SerializeField] private Button buyBtn;
     [SerializeField] private Button donateBtn;
 
+    [Inject]
+    private void Construct(MenuViewModel menuViewModel)
+    {
+        MenuViewModel = menuViewModel;
+    }
+
     private void Start()
     {
+        MenuViewModel.OnOpen += Show;
+    }
 
+    public void Close()
+    {
+        menuPanel.SetActive(false);
+        MenuViewModel.Close();
     }
 
     public void Show()
     {
         menuPanel.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        MenuViewModel.OnOpen -= Show;
     }
 }
