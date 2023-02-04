043109e7fb7219b2c97118df6b87972826258fbb
Hlib <21057497+Glebovvski@users.noreply.github.com>
2023-02-04 12:59:33 +0200

    Update AdManager.cs

--- Assets/Scripts/Managers/AdManager.cs
+++ Assets/Scripts/Managers/AdManager.cs
@@ -5,26 +5,28 @@ using Zenject;
 
 public class AdManager : IInitializable, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
 {
-    private MenuViewModel MenuViewModel { get; set; }
     private GameControlModel GameControlModel { get; set; }
 
 
-    private InterstitialAd interstitialAd;
+    BannerPosition _bannerPosition = BannerPosition.TOP_CENTER;
 
-    public string myGameIdAndroid = "5145689";
-    public string myGameIdIOS = "5145688";
+    private string myGameIdAndroid = "5145689";
+    private string myGameIdIOS = "5145688";
 
-    public string adUnitIdAndroid = "Interstitial_Android";
-    public string adUnitIdIOS = "Interstitial_iOS";
+    private string interstitial_ANDROID = "Interstitial_Android";
+    private string interstitial_IOS = "Interstitial_iOS";
 
-    public string myAdUnitId;
+    private string banner_ANDROID = "Banner_Android";
+    private string banner_IOS = "Banner_IOS";
+
+    private string interstitial;
+    private string banner;
 
     private bool testMode = true;
 
     [Inject]
-    private void Construct(MenuViewModel menuViewModel, GameControlModel gameControlModel)
+    private void Construct(GameControlModel gameControlModel)
     {
-        MenuViewModel = menuViewModel;
         GameControlModel = gameControlModel;
     }
 
@@ -33,61 +35,70 @@ public class AdManager : IInitializable, IUnityAdsInitializationListener, IUnity
 #if UNITY_IOS
             Advertisement.Initialize(myGameIdIOS, testMode, this);
             myAdUnitId = adUnitIdIOS;
+            banner = banner_ANDROID;
 #else
         Advertisement.Initialize(myGameIdAndroid, testMode, this);
-        myAdUnitId = adUnitIdAndroid;
+        interstitial = interstitial_ANDROID;
+        banner = banner_ANDROID;
 #endif
 
+        LoadAndShowBanner();
         GameControlModel.OnRestart += ShowInterstitialAd;
-        MenuViewModel.OnClose += ShowInterstitialAd;
     }
 
     private void ShowInterstitialAd()
     {
         if (Advertisement.isInitialized)
         {
-            Advertisement.Load(myAdUnitId, this);
-            Advertisement.Show(myAdUnitId, this);
+            Advertisement.Show(interstitial, this);
         }
+    }
+
+    private void LoadAndShowBanner()
+    {
+        Advertisement.Banner.SetPosition(_bannerPosition);
+        Advertisement.Banner.Load(banner);
+        Advertisement.Banner.Show(banner);
     }
 
     public void OnInitializationComplete()
     {
-        Debug.LogError("INIT COMPLETE");
+        // Debug.LogError("INIT COMPLETE");
     }
 
     public void OnInitializationFailed(UnityAdsInitializationError error, string message)
     {
-        Debug.LogError("INIT FAILED");
+        // Debug.LogError("INIT FAILED");
     }
 
     public void OnUnityAdsAdLoaded(string placementId)
     {
-        Debug.LogError("AD LOADED");
+        // Debug.LogError("AD LOADED");
     }
 
     public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
     {
-        Debug.LogError("AD FAILED TO LOAD");
+        // Debug.LogError("AD FAILED TO LOAD");
     }
 
     public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
     {
-        Debug.LogError("FAILED TO SHOW AD");
+        // Debug.LogError("FAILED TO SHOW AD");
     }
 
     public void OnUnityAdsShowStart(string placementId)
     {
-        Debug.LogError("STARTED TO SHOW AD");
+        // Debug.LogError("STARTED TO SHOW AD");
     }
 
     public void OnUnityAdsShowClick(string placementId)
     {
-        Debug.LogError("CLICKED ON AD");
+        // Debug.LogError("CLICKED ON AD");
     }
 
     public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
     {
-        Debug.LogError("COMPLETED SHOWING AD");
+        // Debug.LogError("COMPLETED SHOWING AD");
+        Advertisement.Load(interstitial, this);
     }
 }
