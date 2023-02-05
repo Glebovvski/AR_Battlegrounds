586ef4f3c98081582b5cb9cfcf3a0829bf8bebb4
Hlib <21057497+Glebovvski@users.noreply.github.com>
2023-02-05 09:11:15 +0200

    Update AdManager.cs

--- Assets/Scripts/Managers/AdManager.cs
+++ Assets/Scripts/Managers/AdManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Advertisements;
 using Zenject;
 
-public class AdManager : IInitializable, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+public class AdManager : IInitializable
 {
     private GameControlModel GameControlModel { get; set; }
 
@@ -12,20 +12,6 @@ public class AdManager : IInitializable, IUnityAdsInitializationListener, IUnity
     private InterstitialAd interstitialAd;
 
     BannerPosition _bannerPosition = BannerPosition.TOP_CENTER;
-
-    private string myGameIdAndroid = "5145689";
-    private string myGameIdIOS = "5145688";
-
-    private string interstitial_ANDROID = "Interstitial_Android";
-    private string interstitial_IOS = "Interstitial_iOS";
-
-    private string banner_ANDROID = "Banner_Android";
-    private string banner_IOS = "Banner_IOS";
-
-    private string interstitial;
-    private string banner;
-
-    private bool testMode = true;
 
     [Inject]
     private void Construct(GameControlModel gameControlModel)
@@ -36,17 +22,6 @@ public class AdManager : IInitializable, IUnityAdsInitializationListener, IUnity
     public void Initialize()
     {
         MobileAds.Initialize(initStatus => { });
-        // #if UNITY_IOS
-        //             Advertisement.Initialize(myGameIdIOS, testMode, this);
-        //             myAdUnitId = adUnitIdIOS;
-        //             banner = banner_ANDROID;
-        // #else
-        //         Advertisement.Initialize(myGameIdAndroid, testMode, this);
-        //         interstitial = interstitial_ANDROID;
-        //         banner = banner_ANDROID;
-        // #endif
-
-        // LoadAndShowBanner();
         RequestInterstitial();
         RequestBanner();
         GameControlModel.OnRestart += ShowInterstitialAd;
@@ -56,10 +31,6 @@ public class AdManager : IInitializable, IUnityAdsInitializationListener, IUnity
     {
         if (interstitialAd.CanShowAd())
             interstitialAd.Show();
-        // if (Advertisement.isInitialized)
-        // {
-        //     Advertisement.Show(interstitial, this);
-        // }
     }
 
     private void RequestBanner()
@@ -104,53 +75,5 @@ public class AdManager : IInitializable, IUnityAdsInitializationListener, IUnity
         interstitialAd.OnAdFullScreenContentClosed -= RequestInterstitial;
 
         OnInterstitialAdClosed?.Invoke();
-    }
-
-    private void LoadAndShowBanner()
-    {
-        Advertisement.Banner.SetPosition(_bannerPosition);
-        Advertisement.Banner.Load(banner);
-        Advertisement.Banner.Show(banner);
-    }
-
-    public void OnInitializationComplete()
-    {
-        // Debug.LogError("INIT COMPLETE");
-    }
-
-    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
-    {
-        // Debug.LogError("INIT FAILED");
-    }
-
-    public void OnUnityAdsAdLoaded(string placementId)
-    {
-        // Debug.LogError("AD LOADED");
-    }
-
-    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
-    {
-        // Debug.LogError("AD FAILED TO LOAD");
-    }
-
-    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
-    {
-        // Debug.LogError("FAILED TO SHOW AD");
-    }
-
-    public void OnUnityAdsShowStart(string placementId)
-    {
-        // Debug.LogError("STARTED TO SHOW AD");
-    }
-
-    public void OnUnityAdsShowClick(string placementId)
-    {
-        // Debug.LogError("CLICKED ON AD");
-    }
-
-    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
-    {
-        // Debug.LogError("COMPLETED SHOWING AD");
-        Advertisement.Load(interstitial, this);
     }
 }
